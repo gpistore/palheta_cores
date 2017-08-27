4228251bb7bae449436d1967ae97348005471e44
Gustavo Pistore <thetunner@hotmail.com>
2017-08-27 17:55:16 -0300

    v1.1

--- src/view/tela.java
+++ src/view/tela.java
@@ -9,61 +9,48 @@ import org.eclipse.wb.swing.FocusTraversalOnArray;
 import java.awt.Component;
 import java.awt.GridLayout;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.ComponentOrientation;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.border.Border;
+
 import java.awt.Color;
+import java.awt.Dialog.ModalExclusionType;
+import java.awt.Window.Type;
+import javax.swing.border.LineBorder;
+import javax.swing.colorchooser.AbstractColorChooserPanel;
 
 public class tela extends JFrame {
 			JFrame Janela = new JFrame();
 			JScrollPane scroll = new JScrollPane();
 			JMenuBar menuBar = new JMenuBar();
 			JMenu mnArquivo = new JMenu("Arquivo");
-			JMenuItem preferenciasAction = new JMenuItem("Preferencias"), 
+			JMenuItem sobreAction = new JMenuItem("Sobre"), 
 					  sairAction = new JMenuItem("Sair"),
 					  novoAction = new JMenuItem("Novo");
-			private final JPanel panel = new JPanel();
-			private final JLabel lblintro = new JLabel("Programa desenvolvido para efetuar o cálculo de palhetas de cores através do uso de algoritmos genéticos.");
-			private final JButton btnNewButton_1 = new JButton("Calcular");
-			private final JPanel cor1 = new JPanel();
-			private final JPanel cor2 = new JPanel();
-			private final JPanel cor3 = new JPanel();
-			private final JPanel cor4 = new JPanel();
-			private final JPanel cor5 = new JPanel();
-			private final JPanel cor6 = new JPanel();
-			private final JPanel cor7 = new JPanel();
-			private final JPanel cor8 = new JPanel();
-			private final JPanel cor9 = new JPanel();
+			JDialog janelaSobre = new JDialog();
+			private final JButton btncalcular = new JButton("Calcular");
 			private final JPanel [] cores;
 			private final JPanel panelProgresso = new JPanel();
+			private final JPanel panelcor1,panelcor2,panelcor3,panelcor4,panelcor5;
+
+			
+			 private Color color = Color.LIGHT_GRAY;
+			    private JPanel colorJPanel;
 				
 	public tela() {
 				super("Gerador de Palheta de cores");
+				panelProgresso.setBounds(1059, 302, 130, 94);
 				panelProgresso.setBackground(Color.WHITE);
-				cor1.setBackground(Color.WHITE);
-				cor2.setBackground(Color.WHITE);
-				cor3.setBackground(Color.WHITE);
-				cor4.setBackground(Color.WHITE);
-				cor5.setBackground(Color.WHITE);
-				cor6.setBackground(Color.WHITE);
-				cor7.setBackground(Color.WHITE);
-				cor8.setBackground(Color.WHITE);
-				cor9.setBackground(Color.WHITE);
+
 				cores = new JPanel[9];
-				cores[0] = cor1;
-				cores[1] = cor2;
-				cores[2] = cor3;
-				cores[3] = cor4;
-				cores[4] = cor5;
-				cores[5] = cor6;
-				cores[6] = cor7;
-				cores[7] = cor8;
-				cores[8] = cor9;
 				
-				menuprincipal();
-				adicionatela();
-				Janela.setMinimumSize(new Dimension(800, 600));
-				Janela.setMaximumSize(new Dimension(800, 600));
+				sobre();
+				menuPrincipal();
+				adicionaTela();
+				Janela.setMinimumSize(new Dimension(800,600));
+				Janela.setMaximumSize(new Dimension(800,600));
 				Janela.setResizable(false);
 				Janela.setTitle("Gerador de Palheta de cores");
 				Janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
@@ -71,93 +58,110 @@ public class tela extends JFrame {
 				Janela.setVisible(true);
 				Janela.setJMenuBar(menuBar);
 				
-				menuBar.add(panel);
+				panelcor1 = new JPanel();
+				panelcor1.setBackground(Color.WHITE);
+				panelcor1.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+				panelcor1.setBounds(35, 20, 130, 50);
 				
-				JPanel panel_1 = new JPanel();
 				
-				JPanel panel_2 = new JPanel();
-				panel_2.setBackground(Color.WHITE);
-				
-				JButton btnNewButton = new JButton("Selecione");
-				btnNewButton.addActionListener(new ActionListener() {
+				panelcor2 = new JPanel();
+				panelcor2.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+				panelcor2.setBackground(Color.WHITE);
+				panelcor2.setBounds(185, 20, 130, 50);
+				JButton btncor2 = new JButton("Selecione");
+				btncor2.setBounds(185, 77, 130, 23);
+				btncor2.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent arg0) {
+						selecionaCor(panelcor2);
 					}
 				});
-				GroupLayout groupLayout = new GroupLayout(Janela.getContentPane());
-				groupLayout.setHorizontalGroup(
-					groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(panel_1, GroupLayout.DEFAULT_SIZE, 952, Short.MAX_VALUE)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(357)
-							.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-								.addComponent(btnNewButton, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 187, Short.MAX_VALUE)
-								.addComponent(panel_2, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 187, Short.MAX_VALUE))
-							.addGap(113)
-							.addComponent(panelProgresso, GroupLayout.PREFERRED_SIZE, 130, GroupLayout.PREFERRED_SIZE)
-							.addGap(165))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(24)
-							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
-								.addComponent(btnNewButton_1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-								.addGroup(groupLayout.createSequentialGroup()
-									.addComponent(cor1, GroupLayout.PREFERRED_SIZE, 94, GroupLayout.PREFERRED_SIZE)
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(cor2, GroupLayout.PREFERRED_SIZE, 94, GroupLayout.PREFERRED_SIZE)
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(cor3, GroupLayout.PREFERRED_SIZE, 94, GroupLayout.PREFERRED_SIZE)
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(cor4, GroupLayout.PREFERRED_SIZE, 94, GroupLayout.PREFERRED_SIZE)
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(cor5, GroupLayout.PREFERRED_SIZE, 94, GroupLayout.PREFERRED_SIZE)
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(cor6, GroupLayout.PREFERRED_SIZE, 94, GroupLayout.PREFERRED_SIZE)
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(cor7, GroupLayout.PREFERRED_SIZE, 94, GroupLayout.PREFERRED_SIZE)
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(cor8, GroupLayout.PREFERRED_SIZE, 94, GroupLayout.PREFERRED_SIZE)
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(cor9, GroupLayout.PREFERRED_SIZE, 94, GroupLayout.PREFERRED_SIZE)))
-							.addContainerGap(34, Short.MAX_VALUE))
-				);
-				groupLayout.setVerticalGroup(
-					groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 26, GroupLayout.PREFERRED_SIZE)
-							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-								.addGroup(groupLayout.createSequentialGroup()
-									.addGap(30)
-									.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, 56, GroupLayout.PREFERRED_SIZE)
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(btnNewButton)
-									.addGap(61))
-								.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
-									.addPreferredGap(ComponentPlacement.RELATED, 63, Short.MAX_VALUE)
-									.addComponent(panelProgresso, GroupLayout.PREFERRED_SIZE, 94, GroupLayout.PREFERRED_SIZE)
-									.addGap(50)))
-							.addComponent(btnNewButton_1, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-								.addComponent(cor1, GroupLayout.DEFAULT_SIZE, 265, Short.MAX_VALUE)
-								.addComponent(cor2, GroupLayout.PREFERRED_SIZE, 265, GroupLayout.PREFERRED_SIZE)
-								.addComponent(cor3, GroupLayout.PREFERRED_SIZE, 265, GroupLayout.PREFERRED_SIZE)
-								.addComponent(cor4, GroupLayout.PREFERRED_SIZE, 265, GroupLayout.PREFERRED_SIZE)
-								.addComponent(cor5, GroupLayout.PREFERRED_SIZE, 265, GroupLayout.PREFERRED_SIZE)
-								.addComponent(cor6, GroupLayout.PREFERRED_SIZE, 265, GroupLayout.PREFERRED_SIZE)
-								.addComponent(cor7, GroupLayout.PREFERRED_SIZE, 265, GroupLayout.PREFERRED_SIZE)
-								.addComponent(cor8, GroupLayout.PREFERRED_SIZE, 265, GroupLayout.PREFERRED_SIZE)
-								.addComponent(cor9, GroupLayout.PREFERRED_SIZE, 265, GroupLayout.PREFERRED_SIZE))
-							.addContainerGap())
-				);
 				
-				panel_1.add(lblintro);
-				Janela.getContentPane().setLayout(groupLayout);
+				
+				panelcor3 = new JPanel();
+				panelcor3.setBackground(Color.WHITE);
+				panelcor3.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+				panelcor3.setBounds(335, 20, 130, 50);
+				JButton btncor3 = new JButton("Selecione");
+				btncor3.setBounds(335, 77, 130, 23);
+				btncor3.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						selecionaCor(panelcor3);
+					}
+				});
+				
+				panelcor4 = new JPanel();
+				panelcor4.setBackground(Color.WHITE);
+				panelcor4.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+				panelcor4.setBounds(485, 20, 130, 50);
+				JButton btncor4 = new JButton("Selecione");
+				btncor4.setBounds(485, 77, 130, 23);
+				btncor4.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						selecionaCor(panelcor4);
+					}
+				});
+				panelcor5 = new JPanel();
+				panelcor5.setBackground(Color.WHITE);
+				panelcor5.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+				panelcor5.setBounds(635,20, 130, 50);;
+				JButton btncor5 = new JButton("Selecione");
+				btncor5.setBounds(635, 77, 130, 23);
+				btncor5.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						selecionaCor(panelcor5);
+					}
+				});
+				Janela.getContentPane().setLayout(null);
+				Janela.getContentPane().add(panelProgresso);
+				Janela.getContentPane().add(panelcor1);
+				Janela.getContentPane().add(panelcor2);
+				Janela.getContentPane().add(panelcor3);
+				Janela.getContentPane().add(panelcor4);
+				Janela.getContentPane().add(panelcor5);
+				
+								JButton btncor1 = new JButton("Selecione");
+								btncor1.setBounds(35, 77, 130, 23);
+								btncor1.addActionListener(new ActionListener() {
+									public void actionPerformed(ActionEvent arg0) {
+										selecionaCor(panelcor1);
+									}
+								});
+								Janela.getContentPane().add(btncor1);
+				Janela.getContentPane().add(btncor2);
+				Janela.getContentPane().add(btncor3);
+				Janela.getContentPane().add(btncor4);
+				Janela.getContentPane().add(btncor5);
+				
+				btncalcular.setBounds(10, 135, 774, 35);
+				Janela.getContentPane().add(btncalcular);
+				
+				JPanel panelresult = new JPanel();
+				panelresult.setBackground(Color.WHITE);
+				panelresult.setBounds(10, 179, 774, 360);
+				Janela.getContentPane().add(panelresult);
 				
 			}
 			
+			private void sobre() {
+				 JPanel messagePane = new JPanel();
+				 
+		
+				 JLabel texto = new JLabel("<html><body>  Programa desenvolvido para efetuar  o cálculo  de palhetas <br> de cores através do uso de algoritmos genéticos<br><br> Desenvolvido por: <br> Gustavo Pistore <br> Leonardo Gobi <br> Guilherme Masiero </body></html>");
+				 
+				// \b \f \n \r \t
+				 //JLabel texto = new JLabel("Programa \n\n desenvolvido para ");
+		         messagePane.add(texto);
+		         janelaSobre.getContentPane().add(messagePane);
+		         janelaSobre.setTitle("Sobre");
+	                Dimension d = new Dimension();
+	                d.setSize(400, 200);
+	             janelaSobre.setMinimumSize(d);
+			}
+	
 	private void menuArquivo(){
 		menuBar.add(mnArquivo);
 		addmenu(novoAction,mnArquivo);
-		addmenu(preferenciasAction,mnArquivo);
+		addmenu(sobreAction,mnArquivo);
 		addmenu(sairAction,mnArquivo);
 		
 		sairAction.addActionListener(new ActionListener() {
@@ -167,18 +171,37 @@ public class tela extends JFrame {
             }
         });
 		
+		sobreAction.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+            	
+            	
+                janelaSobre.setVisible(true);            	
+            }
+        });
+		
 		
 	}
 			
-	private void menuprincipal(){
+	private void menuPrincipal(){
 		menuArquivo();
 	}
 			
-	private void adicionatela(){
+	private void adicionaTela(){
 	}
 			
 	private void addmenu(JMenuItem filho, JMenu pai){
 		pai.add(filho);
+	}
+	
+	private void selecionaCor(JPanel painel) {
+		JColorChooser seletor = new JColorChooser(painel.getBackground());
+        AbstractColorChooserPanel[] panels = seletor.getChooserPanels();
+        for (AbstractColorChooserPanel accp : panels) {
+            if (accp.getDisplayName().equals("RGB")) {
+                JOptionPane.showMessageDialog(painel, accp);
+            }
+        }
+        painel.setBackground(seletor.getColor());
 	}
 	
 	public void setaCorDegrade(int [] cores) {
@@ -195,5 +218,4 @@ public class tela extends JFrame {
 		this.panelProgresso.setBackground(new Color(cor));
 		this.panelProgresso.repaint();
 	}
-	
 }
